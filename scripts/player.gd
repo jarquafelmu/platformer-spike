e44e3d2d61a1f34189e998b74fcb2d809a037486
Sam Terrazas <thechaoselement@gmail.com>
2026-09-06 19:17:04 -0600

    initial commit

---
+++ scripts/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@export var speed: float = 3_000.0
+@export var jump_velocity: float = -450.0
+@export var acceleration: float = 1_000.0
+@export var deceleration: float = 2_200.0
+
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	var direction := Input.get_axis("move_left", "move_right")
+	var target_speed := direction * speed
+	var rate := acceleration if direction != 0.0 else deceleration
+	
+	velocity.x =  move_toward(
+		velocity.x,
+		target_speed,
+		rate * delta
+	)
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+		
+	move_and_slide()
