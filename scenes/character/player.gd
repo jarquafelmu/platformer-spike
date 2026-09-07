905012c3acbb250439664728375cdbf01bb7bdbd
Sam Terrazas <thechaoselement@gmail.com>
2026-09-07 13:13:25 -0600

    feat: add midair jump mechanic

--- scenes/character/player.gd
+++ scenes/character/player.gd
@@ -16,12 +16,17 @@ const ONE_WAY_PLATFORM_LAYER: int = 3
 var collectible_count: int = 0
 var is_respawning: bool = false
 var is_dropping_through: bool = false
+var air_jump_available: bool = false
 
 
 func _physics_process(delta: float) -> void:
-	if not is_on_floor():
+	var grounded := is_on_floor()
+	
+	if grounded:
+		air_jump_available = true
+	else: 
 		velocity += get_gravity() * delta
-		
+	
 	var direction := Input.get_axis("move_left", "move_right")
 	var target_speed := direction * speed
 	var rate := acceleration if direction != 0.0 else deceleration
@@ -32,17 +37,24 @@ func _physics_process(delta: float) -> void:
 		rate * delta
 	)
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		if Input.is_action_pressed("move_down"):
-			drop_through_platform()
-		else:
-			velocity.y = jump_velocity
-		
+	if Input.is_action_just_pressed("jump"):
+		if grounded:
+			if Input.is_action_pressed("move_down"):
+				drop_through_platform()
+			else:
+				_perform_jump()
+		elif air_jump_available:
+			_perform_jump()
+			air_jump_available = false
+	
 	move_and_slide()
 	
 	if global_position.y > fall_limit:
 		die()
-		
+	
+func _perform_jump() -> void:
+	velocity.y = jump_velocity
+
 func die() -> void:
 	if is_respawning: 
 		return
