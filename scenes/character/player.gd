8b76c34770b40f08b25a1c02e475c3b44f6fe738
Sam Terrazas <thechaoselement@gmail.com>
2026-09-06 22:40:54 -0600

    feat: add platform drop through

--- scenes/character/player.gd
+++ scenes/character/player.gd
@@ -1,15 +1,21 @@
 extends CharacterBody2D
 
+signal collectible_count_changed(new_count: int)
+
 @export var speed: float = 3_000.0
 @export var jump_velocity: float = -450.0
 @export var acceleration: float = 1_000.0
 @export var deceleration: float = 2_200.0
 @export var fall_limit: float = 800.0
+@export var drop_through_duration: float = 0.25
+@export var drop_velocity: float = 100.0
 
-signal collectible_count_changed(new_count: int)
+const ONE_WAY_PLATFORM_LAYER: int = 2
 
 var collectible_count: int = 0
 var is_respawning: bool = false
+var is_dropping_through: bool = false
+
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -26,7 +32,10 @@ func _physics_process(delta: float) -> void:
 	)
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
+		if Input.is_action_pressed("move_down"):
+			drop_through_platform()
+		else:
+			velocity.y = jump_velocity
 		
 	move_and_slide()
 	
@@ -43,3 +52,16 @@ func reset_player() -> void:
 func collect_item(value: int) -> void:
 	collectible_count += value
 	collectible_count_changed.emit(collectible_count)
+	
+func drop_through_platform() -> void:
+	if is_dropping_through:
+		return
+	
+	is_dropping_through = true
+	set_collision_mask_value(ONE_WAY_PLATFORM_LAYER, false)
+	velocity.y = drop_velocity
+	
+	await get_tree().create_timer(drop_through_duration).timeout
+	
+	set_collision_mask_value(ONE_WAY_PLATFORM_LAYER, true)
+	is_dropping_through = false
